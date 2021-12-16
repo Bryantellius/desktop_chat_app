848e785459d1ed148f2e235120a372f21593f011
Ben Bryant <brbryant2639@gmail.com>
2021-12-16 09:56:32 -0600

    UPDATE: Added currentRace support

--- app/src/pages/chatapp/chatapp.jsx
+++ app/src/pages/chatapp/chatapp.jsx
@@ -18,12 +18,16 @@ class ChatApp extends React.Component {
       status: "disconnected",
       running: false,
       time: 0,
+      loadedMeet: {},
+      currentRace: {},
     };
 
     this.startTime = this.startTime.bind(this);
     this.stopTime = this.stopTime.bind(this);
     this.resetTime = this.resetTime.bind(this);
     this.addResult = this.addResult.bind(this);
+    this.loadMeet = this.loadMeet.bind(this);
+    this.selectCurrentRace = this.selectCurrentRace.bind(this);
   }
 
   componentDidMount() {
@@ -41,10 +45,19 @@ class ChatApp extends React.Component {
       console.log(this.socket.id);
       this.setState({ status: "disconnected" });
     });
+
+    this.loadMeet();
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
+  }
+
+  loadMeet() {
+    fetch("http://localhost:3001/api/v1/load-meet")
+      .then((res) => res.json())
+      .then((data) => this.setState({ loadedMeet: data }))
+      .catch((err) => alert(err));
   }
 
   startTime() {
@@ -76,13 +89,25 @@ class ChatApp extends React.Component {
     clearInterval(this.timer);
   }
 
+  selectCurrentRace(e) {
+    let currentRace = this.state.loadedMeet.races.find(
+      (val) => val.id == e.target.value
+    );
+    this.setState({ currentRace });
+    this.socket.emit("race-selection", currentRace);
+  }
+
   addResult(e) {
     e.preventDefault();
 
     let athleteId = document.getElementById("athleteId");
 
+    let athlete = this.state.currentRace.entries.find(
+      (val) => val.athleteId == athleteId.value
+    );
+
     this.socket.emit("result", {
-      athlete: athleteId.value,
+      athlete,
       time: dayjs()
         .minute(0)
         .second(this.state.time / 10)
@@ -105,13 +130,37 @@ class ChatApp extends React.Component {
               }`}>
               {this.state.status == "disconnected" ? "Not" : ""} Connected
             </span>
-            <h1 className="title is-1">Live Results</h1>
+            <h1 className="title is-1">
+              {this.state.currentRace?.title || "Live Results"}
+            </h1>
             <div className="subtitle">
               {dayjs()
                 .hour(0)
                 .minute(0)
                 .second(this.state.time ? this.state.time / 10 : 0)
                 .format("mm:ss")}
+            </div>
+            <div className="control has-icons-left">
+              <div className="select">
+                <select onChange={this.selectCurrentRace}>
+                  <option selected>Select a race..</option>
+                  {this.state.loadedMeet.races?.map((race, idx) => (
+                    <option key={idx} value={race.id}>
+                      {race.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="icon is-small is-left">
+                <i className="fas fa-globe"></i>
+              </div>
+            </div>
+            <div className="control">
+              <button
+                className={`button is-link is-dark`}
+                onClick={this.loadMeet}>
+                Load
+              </button>
             </div>
             <div className="field is-grouped">
               <div className="control">
@@ -149,7 +198,10 @@ class ChatApp extends React.Component {
                 />
               </div>
               <div className="control">
-                <button type="submit" className="button is-info">
+                <button
+                  type="submit"
+                  className="button is-info"
+                  disabled={!this.state.running}>
                   Time
                 </button>
               </div>
